4c55d2ff1c51820c4fa9a1909150d86a9924ddec
HystericLamp <159586448+HystericLamp@users.noreply.github.com>
2025-04-21 15:40:21 -0600

    Start of the Spring project

---
+++ src/main/java/com/cyoa/cyoa_app/CyoaAppApplication.java
@@ -0,0 +1,13 @@
+package com.cyoa.cyoa_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyoaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyoaAppApplication.class, args);
+	}
+
+}
